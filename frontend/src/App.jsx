5d48b43dcd41231a6f5e0cb078714d7dfa82cfd0
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-30 14:15:43 +0900

    073025

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,23 @@
 import './App.css'
 // import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   /*const movieNumber = 1 */ /* rendering condition */
 
   return (
-    <> 
-      <Home />
-    
-    
+    <main className="main-content"> 
+      <Routes></Routes>
+
+
     
     {/* { {movieNumber === 1 ? (
       <MovieCard movie={{title: "Tims Film", release_date: "2024"}} />
     ) : (
       <MovieCard movie={{title: "Joes Film", release_date: "2020"}} />
     )} } */}
-    </>
+    </main>
   );
 }
 

34582879bf17ee99ac06ee7f7da80ca4333a0cfd
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-30 12:54:47 +0900

    073025

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,19 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCard from './components/MovieCard';
 
 function App() {
+  const movieNumber = 1 /* rendering condition */
+
   return (
     <> 
-      <Text />
-      <Text />
+    {movieNumber === 1 ? ( /* getting the movieNumber starting */
+      <MovieCard movie={{title: "Tims Film", release_date: "2024"}} />
+    ) : (
+      <MovieCard movie={{title: "Joes Film", release_date: "2020"}} />
+    )}
     </>
-  );
-}
-
-function Text({text}) {
-  return (
-    <div>
-      <p>hello World</p>
-    </div>
   );
 }
 

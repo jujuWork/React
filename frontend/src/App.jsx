67e49b112a2bdab212a988fa2c945c225a009ee2
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-30 11:12:20 +0900

    073025

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <> 
+      <Text />
+      <Text />
+    </>
+  );
+}
+
+function Text({text}) {
+  return (
+    <div>
+      <p>hello World</p>
+    </div>
+  );
+}
+
+export default App

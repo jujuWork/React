6187851d76c345357f8cc7454a998ba653c2e737
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-31 14:52:58 +0900

    073125

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,15 +1,28 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/NavBar.css";
 
 function Home() {
     const [searchQuery, setSearcgQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        {id:1, title: "John Wick", release_date: "2020"},
-        {id:2, title: "Terminator", release_date: "2021"},
-        {id:3, title: "Godzilla", release_date: "2023"},
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                setError("Failed to Load Movies");
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+            setLoading();
+    }, []);
 
     const handleSearch = () => {
         alert(searchQuery)

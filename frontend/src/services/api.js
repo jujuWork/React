c181b04054e46dee873674a20fee45f7840c2675
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-07-31 14:34:07 +0900

    073125

---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "b698bac1248e240e83e241417c1f5795";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.result
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.result
+};

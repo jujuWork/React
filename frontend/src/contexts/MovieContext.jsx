e33cd8c0797d9c52c43f866c6baaf70881ca8530
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-08-03 12:58:59 +0900

    080325

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -7,16 +7,33 @@ export const useMovieContext = () => useContext (MovieContext)
 export const MovieProvider = ({children}) => {
     const [favorites, setFavorites] = useState([])
 
+        {/* Storing the Favorites Movie */}
     useEffect(() => {
         const storedFavs = localStorage.getItem("favorites")
 
         if (storedFavs) setFavorites(JSON.parse(storedFavs))
     }, [])
 
+        {/* Updating the Favorite Movie */}
     useEffect(() => {
         localStorage.setItem('favorites', JSON.stringify(favorites))
     }, [favorites])
-    
+
+        {/* Adding Movie to Favorites */}
+    const addToFavorites = (movie) => {
+        setFavorites(prev => [...prev, movie])
+    }
+
+        {/* Removing Movies from Favorites */}
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+
+        {/* Checking if all ID is the same as movieId */}
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movie.id === movieId)
+    }
+
     return <MovieContext.Provider>
         {children}
     </MovieContext.Provider>
